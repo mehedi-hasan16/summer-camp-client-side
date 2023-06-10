118f36298fb4b8aae75d38526637e467d640933e
mehedi-hasan16 <hasanmehedi6541@gmail.com>
2023-06-11 01:40:58 +0600

    added popular class in homepage

--- src/components/PopularClass/PopularClass.jsx
+++ src/components/PopularClass/PopularClass.jsx
@@ -1,13 +1,24 @@
 import { useEffect, useState } from "react";
 import SectionName from "../SectionName/SectionName";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
+import { useQuery } from "@tanstack/react-query";
 
 const PopularClass = () => {
-    const [classes, setClass] = useState([])
-    useEffect(() => {
-        fetch('http://localhost:5000/classes')
-            .then(res => res.json())
-            .then(data => setClass(data))
-    }, [])
+    // const [classes, setClass] = useState([])
+    // useEffect(() => {
+    //     fetch('http://localhost:5000/classes')
+    //         .then(res => res.json())
+    //         .then(data => setClass(data))
+    // }, [])
+    const [axiosSecure] = useAxiosSecure();
+
+    const { data: data = [] } = useQuery({
+        queryKey: ['popularClasses'],
+        queryFn: async () => {
+            const res = await axiosSecure.get(`/classes/topSort`);
+            return res.data;
+        }
+    })
     return (
     
         <div>
@@ -15,19 +26,14 @@ const PopularClass = () => {
             <div className="grid grid-cols-3 gap-4 my-10">
             
             {
-                classes.map(item => <div key={item._id}>
+                data.map(item => <div key={item._id}>
                     <div className="card w-96 bg-base-100 shadow-xl">
-                        <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+                        <figure><img src={item.image} alt="image" /></figure>
                         <div className="card-body">
-                            <h2 className="card-title">
-                                Shoes!
-                                <div className="badge badge-secondary">NEW</div>
-                            </h2>
-                            <p>If a dog chews shoes whose shoes does he choose?</p>
-                            <div className="card-actions justify-end">
-                                <div className="badge badge-outline">Fashion</div>
-                                <div className="badge badge-outline">Products</div>
-                            </div>
+                            <h2 className="card-title">{item.className}</h2>
+                            <p>Instructor: {item.instructor}</p>
+                            <p>Enrolled: {item.enrolled}</p>
+                            <p>Price: ${item.price}</p>
                         </div>
                     </div>
                 </div>)
