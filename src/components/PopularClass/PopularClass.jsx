d4b1d6770066de0a5f1a878231b5e56554b8342f
mehedi-hasan16 <hasanmehedi6541@gmail.com>
2023-06-11 11:42:15 +0600

    homepage mobile responsive

--- src/components/PopularClass/PopularClass.jsx
+++ src/components/PopularClass/PopularClass.jsx
@@ -20,25 +20,25 @@ const PopularClass = () => {
         }
     })
     return (
-    
+
         <div>
             <SectionName title='popular classes'></SectionName>
-            <div className="md:grid grid-cols-3 gap-4 my-10">
-            
-            {
-                data.map(item => <div key={item._id}>
-                    <div className="card w-96 bg-base-100 shadow-xl">
-                        <figure><img src={item.image} alt="image" /></figure>
-                        <div className="card-body">
-                            <h2 className="card-title">{item.className}</h2>
-                            <p>Instructor: {item.instructor}</p>
-                            <p>Enrolled: {item.enrolled}</p>
-                            <p>Price: ${item.price}</p>
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-4 my-10 px-4 md:px-0">
+
+                {
+                    data.map(item => <div key={item._id}>
+                        <div className="card md:w-96 bg-base-100 shadow-xl">
+                            <figure><img src={item.image} alt="image" /></figure>
+                            <div className="card-body">
+                                <h2 className="card-title">{item.className}</h2>
+                                <p>Instructor: {item.instructor}</p>
+                                <p>Enrolled: {item.enrolled}</p>
+                                <p>Price: ${item.price}</p>
+                            </div>
                         </div>
-                    </div>
-                </div>)
-            }
-        </div>
+                    </div>)
+                }
+            </div>
         </div>
     );
 };
--- src/components/PopularInstructors/PopularInstructors.jsx
+++ src/components/PopularInstructors/PopularInstructors.jsx
@@ -16,11 +16,11 @@ const PopularInstructors = () => {
     return (
         <div>
             <SectionName title='Popular Instructors'></SectionName>
-            <div className='md:grid grid-cols-3 gap-4 mb-16'>
+            <div className='grid grid-cols-1 md:grid-cols-3 gap-4 my-10 px-4 md:px-0'>
                 {
                     data.map(item => <div key={item._id}>
 
-                        <div className="card card-compact w-96 bg-base-100 shadow-xl">
+                        <div className="card card-compact md:w-96 bg-base-100 shadow-xl">
                             <figure><img src={item.image} alt="image" /></figure>
                             <div className="card-body">
                                 <h2 className="card-title">{item.name}</h2>
